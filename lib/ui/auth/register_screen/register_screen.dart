07aa93b81986abbf2647ceef939ddf10cd2c3bbe
mohamedsamy <modys1106@gmail.com>
2025-08-20 12:20:18 +0300

    Merge remote-tracking branch 'origin/feature/onboarding' into develop
    # Conflicts:
    #	lib/l10n/app_ar.arb
    #	lib/l10n/app_en.arb
    #	lib/l10n/app_localizations.dart
    #	lib/l10n/app_localizations_ar.dart
    #	lib/l10n/app_localizations_en.dart
    #	lib/main.dart
    #	lib/utils/app_assets.dart
    #	lib/utils/app_routes.dart
    #	lib/utils/app_styles.dart
    #	test/widget_test.dart

--- lib/ui/auth/register_screen/register_screen.dart
+++ lib/ui/auth/register_screen/register_screen.dart
@@ -60,182 +60,184 @@ class _RegisterScreenState extends State<RegisterScreen> {
           style: AppStyles.medium16yellow,),
       ),
       body: SafeArea(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        SizedBox(
-          height: height*0.15,
-            child: PageView.builder(
-                  padEnds: true,
-                  controller: controller,
-              itemCount: avatars.length,
-              onPageChanged: (int index) {
-                     setState(() {
-                       currentPage = index;
-                     });
-                  },
-              itemBuilder: (context, index) {
-
-                if (index == currentPage) {
-                  return Image.asset(avatars[index], scale: 0.09,);
-                } else {
-                  return Image.asset(avatars[index], scale: 1.2);
-                }
-
-          },
-            ),
-        ),
-        SizedBox(height: height*0.01,),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(AppLocalizations.of(context)!.avatar,
-              style: AppStyles.medium16White,)
-          ],
-        ),
-        SizedBox(height: height*0.01,),
-
-        Padding(
-
-          padding: EdgeInsetsGeometry.symmetric(horizontal: width*0.044, ),
-
-          child: Form(
-              key: formKey,
-              child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CustomTextField(
-                  controller: nameController,
-                  style: AppStyles.medium16White,
-                  prefixIcon: ImageIcon(AssetImage(AppAssets.namePrefixIcon),color: AppColors.whiteColor,),
-                  hintText: AppLocalizations.of(context)!.name,
-                ),
-                SizedBox(height: height*0.024,),
-
-                CustomTextField(
-                  controller: emailController,
-                  style: AppStyles.medium16White,
-                  prefixIcon: ImageIcon(AssetImage(AppAssets.emailPrefixIcon),color: AppColors.whiteColor,),
-                  hintText: AppLocalizations.of(context)!.email,
-                  validator: (text) {
-                    if(text==null || text.isEmpty){
-                      return AppLocalizations.of(context)!.please_enter_email;
-                    }
-                    final bool emailValid =
-                    RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(text);
-                    if(!emailValid){
-                      return AppLocalizations.of(context)!.please_valid_email;
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: height*0.024,),
-
-                CustomTextField(
-                  controller: passwordController,
-                  style: AppStyles.medium16White,
-                  prefixIcon: ImageIcon(AssetImage(AppAssets.passwordPrefixIcon),color: AppColors.whiteColor,),
-                  suffixIcon:
-                  GestureDetector(
-                      onTap: (){
-                        isObscure = !isObscure;
-                        setState(() {
-
-                        });
-                      },
-                      child: ImageIcon(AssetImage(AppAssets.passwordSuffixIcon),color: AppColors.whiteColor,)),
-                  hintText: AppLocalizations.of(context)!.password,
-                  validator: (text){
-                    if(text==null||text.isEmpty){
-                      return AppLocalizations.of(context)!.please_enter_password;
-                    }
-                    if(text.length<6){
-                      return AppLocalizations.of(context)!.please_valid_password;
-                    }
-                    return null;
-                  },
-                  obscureText: isObscure,
-                ),
-                SizedBox(height: height*0.024,),
-
-                CustomTextField(
-                  controller: confirmPasswordController,
-                  style: AppStyles.medium16White,
-                  prefixIcon: ImageIcon(AssetImage(AppAssets.passwordPrefixIcon),color: AppColors.whiteColor,),
-                  suffixIcon:
-                  GestureDetector(
-                      onTap: (){
-                        isObscureRe = !isObscureRe;
-                        setState(() {
-
-                        });
-                      },
-                      child: ImageIcon(AssetImage(AppAssets.passwordSuffixIcon),color: AppColors.whiteColor,)),
-                  hintText: AppLocalizations.of(context)!.re_password,
-                  validator: (text){
-                    if(text==null||text.isEmpty){
-                      return AppLocalizations.of(context)!.please_enter_password;
-                    }
-                    if(text!= passwordController.text){
-                      return AppLocalizations.of(context)!.please_enter_the_same_password;
-                    }
-                    return null;
-                  },
-                  obscureText: isObscureRe,
-                ),
-                SizedBox(height: height*0.024,),
-                CustomTextField(
-                  controller: phoneController,
-                  style: AppStyles.medium16White,
-                  prefixIcon: ImageIcon(AssetImage(AppAssets.phonePrefixIcon),color: AppColors.whiteColor,),
-                  hintText: AppLocalizations.of(context)!.phone_number,
-                ),
-                SizedBox(height: height*0.024,),
-
-                CustomElevatedButton(
-                    onPressed: (){
-                      //todo: go back to login
-                      login();
+    child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: height*0.15,
+              child: PageView.builder(
+                    padEnds: true,
+                    controller: controller,
+                itemCount: avatars.length,
+                onPageChanged: (int index) {
+                       setState(() {
+                         currentPage = index;
+                       });
                     },
-                    text: AppLocalizations.of(context)!.create_one),
-              ],
-            ),
-          )),
-        ),
-        SizedBox(height: height*0.024,),
-        RichText(
-          textAlign: TextAlign.center,
-          text: TextSpan(
+                itemBuilder: (context, index) {
+      
+                  if (index == currentPage) {
+                    return Image.asset(avatars[index], scale: 0.09,);
+                  } else {
+                    return Image.asset(avatars[index], scale: 1.2);
+                  }
+      
+            },
+              ),
+          ),
+          SizedBox(height: height*0.01,),
+      
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextSpan(
-                text: "${AppLocalizations.of(context)!.already_have_account} ",
-                style: AppStyles.medium14White,
-              ),
-              TextSpan(
-                text: AppLocalizations.of(context)!.login,
-                style: AppStyles.bold14Yellow,
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    //todo:go back to login
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, AppRoutes.loginRouteName, (route) => false);
-                  },
-              ),
+              Text(AppLocalizations.of(context)!.avatar,
+                style: AppStyles.medium16White,)
             ],
           ),
-        ),
-
-        SizedBox(height: height*0.036,),
-
-        AnimatedToggleButtonLanguage()
-
-
-
-      ]
+          SizedBox(height: height*0.01,),
+      
+          Padding(
+      
+            padding: EdgeInsetsGeometry.symmetric(horizontal: width*0.044, ),
+      
+            child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  CustomTextField(
+                    controller: nameController,
+                    style: AppStyles.medium16White,
+                    prefixIcon: ImageIcon(AssetImage(AppAssets.namePrefixIcon),color: AppColors.whiteColor,),
+                    hintText: AppLocalizations.of(context)!.name,
+                  ),
+                  SizedBox(height: height*0.024,),
+      
+                  CustomTextField(
+                    controller: emailController,
+                    style: AppStyles.medium16White,
+                    prefixIcon: ImageIcon(AssetImage(AppAssets.emailPrefixIcon),color: AppColors.whiteColor,),
+                    hintText: AppLocalizations.of(context)!.email,
+                    validator: (text) {
+                      if(text==null || text.isEmpty){
+                        return AppLocalizations.of(context)!.please_enter_email;
+                      }
+                      final bool emailValid =
+                      RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(text);
+                      if(!emailValid){
+                        return AppLocalizations.of(context)!.please_valid_email;
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: height*0.024,),
+      
+                  CustomTextField(
+                    controller: passwordController,
+                    style: AppStyles.medium16White,
+                    prefixIcon: ImageIcon(AssetImage(AppAssets.passwordPrefixIcon),color: AppColors.whiteColor,),
+                    suffixIcon:
+                    GestureDetector(
+                        onTap: (){
+                          isObscure = !isObscure;
+                          setState(() {
+      
+                          });
+                        },
+                        child: ImageIcon(AssetImage(AppAssets.passwordSuffixIcon),color: AppColors.whiteColor,)),
+                    hintText: AppLocalizations.of(context)!.password,
+                    validator: (text){
+                      if(text==null||text.isEmpty){
+                        return AppLocalizations.of(context)!.please_enter_password;
+                      }
+                      if(text.length<6){
+                        return AppLocalizations.of(context)!.please_valid_password;
+                      }
+                      return null;
+                    },
+                    obscureText: isObscure,
+                  ),
+                  SizedBox(height: height*0.024,),
+      
+                  CustomTextField(
+                    controller: confirmPasswordController,
+                    style: AppStyles.medium16White,
+                    prefixIcon: ImageIcon(AssetImage(AppAssets.passwordPrefixIcon),color: AppColors.whiteColor,),
+                    suffixIcon:
+                    GestureDetector(
+                        onTap: (){
+                          isObscureRe = !isObscureRe;
+                          setState(() {
+      
+                          });
+                        },
+                        child: ImageIcon(AssetImage(AppAssets.passwordSuffixIcon),color: AppColors.whiteColor,)),
+                    hintText: AppLocalizations.of(context)!.re_password,
+                    validator: (text){
+                      if(text==null||text.isEmpty){
+                        return AppLocalizations.of(context)!.please_enter_password;
+                      }
+                      if(text!= passwordController.text){
+                        return AppLocalizations.of(context)!.please_enter_the_same_password;
+                      }
+                      return null;
+                    },
+                    obscureText: isObscureRe,
+                  ),
+                  SizedBox(height: height*0.024,),
+                  CustomTextField(
+                    controller: phoneController,
+                    style: AppStyles.medium16White,
+                    prefixIcon: ImageIcon(AssetImage(AppAssets.phonePrefixIcon),color: AppColors.whiteColor,),
+                    hintText: AppLocalizations.of(context)!.phone_number,
+                  ),
+                  SizedBox(height: height*0.024,),
+      
+                  CustomElevatedButton(
+                      onPressed: (){
+                        //todo: go back to login
+                        login();
+                      },
+                      text: AppLocalizations.of(context)!.create_one),
+                ],
+              ),
+            )),
+          ),
+          SizedBox(height: height*0.024,),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "${AppLocalizations.of(context)!.already_have_account} ",
+                  style: AppStyles.medium14White,
+                ),
+                TextSpan(
+                  text: AppLocalizations.of(context)!.login,
+                  style: AppStyles.bold14Yellow,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      //todo:go back to login
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, AppRoutes.loginRouteName, (route) => false);
+                    },
+                ),
+              ],
+            ),
+          ),
+      
+          SizedBox(height: height*0.036,),
+      
+          AnimatedToggleButtonLanguage()
+      
+      
+      
+        ]
+      ),
     ),
 
 
