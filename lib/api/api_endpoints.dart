6d3342509918d3049b93ab9633bf801e67808b01
mohamedsamy <modys1106@gmail.com>
2025-09-01 23:53:29 +0300

    implement getMoviesDetails Function change theme by bloc

--- lib/api/api_endpoints.dart
+++ lib/api/api_endpoints.dart
@@ -1,6 +1,7 @@
 class ApiEndPoints {
   static const String registerEndPoint = '/auth/register';
   static const String movieEndPoint = '/api/v2/list_movies.json';
+  static const String movieDetailsEndPoint = '/api/v2/movie_details.json';
   static const String loginEndPoint = '/auth/login';
   static const String profileEndPoint = '/profile';
   static const String resetPasswordEndPoint = '/auth/reset-password';
